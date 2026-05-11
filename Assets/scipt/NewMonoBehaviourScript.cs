3366c28e46e651f09b83bc0059ad6fc0e409af3f
Names <40932@ma-web.nl>
2026-05-11 11:04:39 +0200

    second commit

---
+++ Assets/scipt/NewMonoBehaviourScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Tilemap tm;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            Vector3 mouspos = Input.mousePosition;
+            Vector3 worldpos = Camera.main.ScreenToWorldPoint(mouspos);
+            Vector3Int cellpos = tm.WorldToCell(worldpos);
+            Debug.Log($" mouspos {mouspos}");
+            Debug.Log($" worldpos {worldpos}" );
+            Debug.Log($" cellpos {cellpos}");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gdv_gym_2d
+
+![s](Assets/image/2345678y.gif)
