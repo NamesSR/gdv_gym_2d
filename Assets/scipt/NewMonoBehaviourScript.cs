e03492e6305c8c89e0fa68ec09e6a976fec54345
Names <40932@ma-web.nl>
2026-05-27 10:13:57 +0200

    2

--- Assets/scipt/NewMonoBehaviourScript.cs
+++ Assets/scipt/NewMonoBehaviourScript.cs
@@ -5,6 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] Tilemap tm;
+    
     void Start()
     {
         
@@ -17,6 +18,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
             Vector3 mouspos = Input.mousePosition;
             Vector3 worldpos = Camera.main.ScreenToWorldPoint(mouspos);
             Vector3Int cellpos = tm.WorldToCell(worldpos);
+            tm.SetTileFlags(cellpos, TileFlags.None);
+            tm.SetColor(cellpos, Color.red);
             Debug.Log($" mouspos {mouspos}");
             Debug.Log($" worldpos {worldpos}" );
             Debug.Log($" cellpos {cellpos}");
