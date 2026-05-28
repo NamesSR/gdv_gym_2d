af3f181b7eba0eec2799fd2fea7c1d32a1501f49
Names <40932@ma-web.nl>
2026-05-28 14:31:17 +0200

    3

--- Assets/scipt/NewMonoBehaviourScript.cs
+++ Assets/scipt/NewMonoBehaviourScript.cs
@@ -1,29 +1,55 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] Tilemap tm;
+    BoundsInt bounds;
     
     void Start()
     {
-        
+        bounds = tm.cellBounds;
+        Debug.Log(bounds);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)){
-            Vector3 mouspos = Input.mousePosition;
-            Vector3 worldpos = Camera.main.ScreenToWorldPoint(mouspos);
-            Vector3Int cellpos = tm.WorldToCell(worldpos);
-            tm.SetTileFlags(cellpos, TileFlags.None);
-            tm.SetColor(cellpos, Color.red);
-            Debug.Log($" mouspos {mouspos}");
-            Debug.Log($" worldpos {worldpos}" );
-            Debug.Log($" cellpos {cellpos}");
+            Vector3 mouspos2 = Input.mousePosition;
+            Vector3 worldpos2 = Camera.main.ScreenToWorldPoint(mouspos2);
+            Vector3Int cellpos3 = tm.WorldToCell(worldpos2);
+            Debug.Log($" mouspos {mouspos2}");
+            Debug.Log($" worldpos {worldpos2}" );
+            Debug.Log($" cellpos {cellpos3}");
 
+        }
+        Vector3 mouspos = Input.mousePosition;
+        Vector3 worldpos = Camera.main.ScreenToWorldPoint(mouspos);
+        Vector3Int cellpos = tm.WorldToCell(worldpos);
+        //tm.SetTileFlags(cellpos, TileFlags.None);
+        //tm.SetColor(cellpos, Color.red);
+        for (int i = 0 + bounds.position.x; i < bounds.size.x; i++)
+        {
+            for (int j = 0 + bounds.position.y; j < bounds.size.y; j++)
+            {
+
+                Vector3Int cellpos2 = new Vector3Int(i, j, 0);
+                tm.SetTileFlags(cellpos2, TileFlags.None);
+                if (cellpos2 == cellpos)
+                {
+
+                    tm.SetColor(cellpos2, Color.red);
+
+
+                }
+                else
+                {
+                    tm.SetColor(cellpos2, Color.white);
+                }
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # gdv_gym_2d
 
 ![s](Assets/image/2345678y.gif)
+![s](Assets/image/3.gif)
